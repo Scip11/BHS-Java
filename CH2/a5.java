700c8b859dd0c3c08ac0c1f2aff72fc3673ca7fe
KingLagalot <naheitsch@gmail.com>
2014-10-22 07:12:35 -0400

    Removed temps, worked on a5

--- CH2/a5.java
+++ CH2/a5.java
@@ -1,5 +1,31 @@
-public class a2 {
+public class a5 {
 	public static void main(String[] args){	
-	
+		SavitchIn savitchIn = new SavitchIn();
+		String sentence;
+		String newSentence;
+		long sentenceLength;
+		long i;
+
+		System.out.println("Write a sentence and then press enter/return.");
+		sentence = savitchIn.readLine();
+
+		System.out.println("	You entered:");
+		System.out.println("	" + sentence);
+
+		sentence = sentence.trim();
+		
+		String[] sentenceArray = sentence.split(" ");
+		
+		i = sentenceArray[].lenght();
+
+		newSentence = sentenceArray[i] + " ";
+		i = 0;
+		while(i < i-1){
+			newSentence = newSentence + sentenceArray[i];
+			i++;
+		}
+
+		System.out.println(sentence);
+
 	}
 }
